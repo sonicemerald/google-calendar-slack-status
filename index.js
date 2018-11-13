3090413ba5620c5681fa2b757af07d0a4b06d543
Micah Gemmell <sonicemerald@gmail.com>
2018-11-13 10:50:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/', (req, res, next) => {
   slack.users.profile.set({
     token: process.env.SLACK_TOKEN,
     profile: JSON.stringify({
-      "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`
+      "status_text": `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`,
       "status_emoji": ':calendar:'
     })
   });
